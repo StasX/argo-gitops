ce5746558a72e543d4a378543ec813541acc038f
StasX <s.mestechkin@gmail.com>
2026-06-11 19:59:42 +0300

    Fix installation scripts

--- config/install_aws_monitor.bat
+++ config/install_aws_monitor.bat
@@ -1,4 +1,4 @@
 @echo off
 kubectl apply -f .\projects\aws-monitor-project.yaml
 kubectl apply -f .\applications\aws-monitor-app.yaml
-kubectl apply -f .\applicationsets\aws-monitor-app.yaml
+kubectl apply -f .\applicationsets\aws-monitor.yaml
--- config/install_aws_monitor.sh
+++ config/install_aws_monitor.sh
@@ -1,3 +1,3 @@
 kubectl apply -f ./projects/aws-monitor-project.yaml
 kubectl apply -f ./applications/aws-monitor-app.yaml
-kubectl apply -f ./applicationsets/aws-monitor-app.yaml
+kubectl apply -f ./applicationsets/aws-monitor.yaml
